ba8e3a8722608ec98efda8d5df3489b52625fdf6
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-02-28 14:32:54 +0630

    Add project files.

---
+++ KafkaPointToPoint.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaPointToPoint.Consumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:29092",
+                GroupId = "test-consumer-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            consumer.Subscribe("test-topic");
+
+            Console.WriteLine("Consumer started...");
+            while (true)
+            {
+                var consumeResult = consumer.Consume(CancellationToken.None);
+                Console.WriteLine($"Received message: {consumeResult.Value}");
+            }
+        }
+    }
+}
---
+++ KafkaPointToPoint.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaPointToPoint.Producer
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:29092"
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            Console.WriteLine("Producer started...");
+
+            for (int i = 0; i < 10; i++)
+            {
+                var deliveryReport = await producer.ProduceAsync("test-topic",
+                    new Message<Null, string> { Value = $"Message {i}" });
+
+                Console.WriteLine($"Produced message: {deliveryReport.Value}");
+            }
+        }
+    }
+}
